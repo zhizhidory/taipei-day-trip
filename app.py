6b4ab8933f5d3c58361db75ffbfb62d453217c84
Dory <a0952862369@gmail.com>
2022-12-01 15:07:40 +0800

    finish the page of attraction

--- app.py
+++ app.py
@@ -131,7 +131,7 @@ def categories_api():
 @app.route("/")
 def index():
 	return render_template("index.html")
-@app.route("/attraction/<id>")
+@app.route("/attraction/<id>", methods=["POST"])
 def attraction(id):
 	return render_template("attraction.html")
 @app.route("/booking")
