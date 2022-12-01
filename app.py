7c69f27bcf1c1d11064c53af9b7d995977605580
Dory <a0952862369@gmail.com>
2022-12-01 15:58:48 +0800

    fix the link from index page to attraction page

--- app.py
+++ app.py
@@ -131,7 +131,7 @@ def categories_api():
 @app.route("/")
 def index():
 	return render_template("index.html")
-@app.route("/attraction/<id>", methods=["POST"])
+@app.route("/attraction/<id>")
 def attraction(id):
 	return render_template("attraction.html")
 @app.route("/booking")
